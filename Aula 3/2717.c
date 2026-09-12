64f7ba5d029d61fc6f33c6f60e254a721534fa45
𝖵𝗍𝗍𝗓𝗄 .ᐟ.ᐟ <vttzk00@gmail.com>
2026-09-12 15:30:01 -0300

    Create 2717.c

---
+++ Aula 3/2717.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    
+    int gabriel, andro, modesto;
+    scanf("%d%d%d", &modesto, &andro, &gabriel);
+    
+    if( gabriel + andro > modesto){
+        printf("Deixa para amanha!\n");
+        }
+        
+    else{
+         printf("Farei hoje!\n");
+         }
+    
+    return 0;
+}
